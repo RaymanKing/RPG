548ef63e77817ca38e95c329991517f3e9aae568
RaymanKing <enrikerapela@gmail.com>
2021-01-20 14:32:09 +0100

    Errores

--- js/verify.js
+++ js/verify.js
@@ -1,26 +1,18 @@
-const URL = 'http://localhost:1337/Users';
+const URL = 'http://localhost:1337/auth/local';
 
-async function getRequest(url){
+async function postRequest(name, pass){
     const options = {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
+        method: 'POST',
+        headers: { 
+            'Content-Type': 'application/json'
         },
+        body:JSON.stringify( {
+            username:name,
+            password:pass
+        }),
     };
-    return fetch(url, options)
-        .then(response => response.json())
-        .then(data => data);
-}
-
-async function getPlayers(name , pass){
-    const result =  await getRequest(URL);
-    console.log('result',result);
-    for(let i = 0; i < result.length; i++){
-        console.log(result[i].username+' '+result[i].jwt)
-        if(result[i].username == name && result[i].password == pass){
-            alert('Has iniciado sesión en RPG!');
-        }else{
-            alert('pepe')
-        }
-    }
+    return fetch(URL, options)
+        .then(response => {
+            console.log('Well done!');
+        });
 }
